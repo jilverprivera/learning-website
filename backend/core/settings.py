b306f0a9279023c4d5a135ca347c32cad8f5e7cf
Jilver Pacheco <jilverrivera1@gmail.com>
2023-01-09 00:05:08 -0500

    add default aws acl

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -97,12 +97,12 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# AWS settings
+# AWS Configuration
 AWS_ACCESS_KEY_ID = env.get_value('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env.get_value('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env.get_value('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = env.get_value('AWS_DEFAULT_ACL')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
@@ -112,13 +112,11 @@ AWS_HEADERS = {
     'Access-Control-Allow-Origin': '*'
 }
 
+# S3 Configuration
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
-
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
